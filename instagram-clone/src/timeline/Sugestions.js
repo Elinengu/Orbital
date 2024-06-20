4908dd5d91ff6f89b8d46b2d72a83371da743684
Eline Ngu Xiang Ee <e1130038@u.nus.edu>
2024-06-21 05:06:45 +0800

    a not-so-completed insta frontend clone

--- instagram-clone/src/timeline/Sugestions.js
+++ instagram-clone/src/timeline/Sugestions.js
@@ -1,8 +1,64 @@
 import React from "react";
 import "./Sugestions.css";
+import { Avatar } from "@mui/material";
 
 function Sugestions() {
-  return <div>Sugestions</div>;
+  return (
+    <div className="suggestions">
+      <div className="suggestions__title">Suggestions for you</div>
+      <div className="suggestion__username">
+        <div className="username__left">
+          <span className="avatar">
+            <Avatar>R</Avatar>
+          </span>
+          <div className="username__info">
+            <span className="username">Eline</span>
+            <span className="relation">New to Instagram</span>
+          </div>
+        </div>
+        <div className="follow__button">Follow</div>
+      </div>
+
+      <div className="suggestion__username">
+        <div className="username__left">
+          <span className="avatar">
+            <Avatar>R</Avatar>
+          </span>
+          <div className="username__info">
+            <span className="username">Eline</span>
+            <span className="relation">New to Instagram</span>
+          </div>
+        </div>
+        <div className="follow__button">Follow</div>
+      </div>
+
+      <div className="suggestion__username">
+        <div className="username__left">
+          <span className="avatar">
+            <Avatar>R</Avatar>
+          </span>
+          <div className="username__info">
+            <span className="username">Eline</span>
+            <span className="relation">New to Instagram</span>
+          </div>
+        </div>
+        <div className="follow__button">Follow</div>
+      </div>
+
+      <div className="suggestion__username">
+        <div className="username__left">
+          <span className="avatar">
+            <Avatar>R</Avatar>
+          </span>
+          <div className="username__info">
+            <span className="username">Eline</span>
+            <span className="relation">New to Instagram</span>
+          </div>
+        </div>
+        <div className="follow__button">Follow</div>
+      </div>
+    </div>
+  );
 }
 
 export default Sugestions;
